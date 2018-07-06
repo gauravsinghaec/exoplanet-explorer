9c0464b2064db9ce28fda600905d5118ccfaef01
gauravsinghaec <gauravsinghaec@gmail.com>
2018-07-06 16:42:48 +0530

    Add multiple planets on page in same order as it is in array.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,8 +64,13 @@ Instructions:
      */
     getJSON('../data/earth-like-results.json')
     .then(function(response) {
+      console.log(response.results);
+      var sequence = Promise.resolve();
       response.results.forEach(function(url) {
-        getJSON(url).then(createPlanetThumb);
+        sequence = sequence.then(function(){
+          return getJSON(url);
+        }).then(createPlanetThumb)
+        ;
       });
     });
   });
